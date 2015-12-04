70ea53ca06c848414fdfd3f0311fe0fc379071e7
Neonila Shlikhtenko <n.shlikhtenko@levi9.com>
2015-12-04 16:54:39 +0200

    Fake the Web Service

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
 (function () {
 	"use strict";
-	var app = angular.module("productManagement", []);
+	
+	var app = angular.module("productManagement",
+							["common.services",
+							"productResourceMock"]);
 }());
